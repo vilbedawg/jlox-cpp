2a741447d8aa656ad1c7f7fac9602c652d2896c2
Vilho Luoma <vilho.luoma@gmail.com>
2022-09-27 16:25:33 +0300

    Added file reading

--- Bis Interpreter/Main.cpp
+++ Bis Interpreter/Main.cpp
@@ -1,4 +1,36 @@
-int main()
+#include <fstream>
+#include <string>
+#include <iostream>
+
+std::string readFile(const std::string_view filename)
 {
+	std::ifstream file{ filename.data(), std::ios::ate };
+	if (!file)
+	{
+		// TODO; Better error message.
+		std::cerr << "Failed to open file " << filename.data() << '\n';
+		std::exit(74); // I/O error
+	}
+
+	std::string file_contents;
+	file_contents.resize(file.tellg());
+	file.seekg(0, std::ios::beg);
+	file.read(file_contents.data(), file_contents.size());
+	file.close();
+	return file_contents;
+}
+
+
+void initFile(const std::string_view filename)
+{
+	std::string file_contents{ readFile(filename) };
+	std::cout << file_contents << '\n';
+	// TODO; Pass the file_contents to the tokenizer
+}
+
+
+int main(int argc, char* argv[])
+{
+	initFile(argv[1]);
 	return 0;
 }
